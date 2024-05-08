bf183646c3c77e44affea16d1ee2e4f394358917
Divyansh Singh <P2727346@my365.dmu.ac.uk>
2024-05-08 16:13:50 +0100

    Test class created for Staff

---
+++ Testing2/tstStaff.cs
@@ -0,0 +1,17 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing2
+{
+    [TestClass]
+    public class tstStaff
+    {
+        [TestMethod]
+        public void InstanceOk()
+        {
+            clsStaff AnStaff = new clsStaff();
+            Assert.IsNotNull( AnStaff );
+        }
+    }
+}
