7cfdbb5a05ebe44596f3c81ea53289f588c1c709
DanHowe0 <39921326+DanHowe0@users.noreply.github.com>
2024-05-08 16:34:03 +0100

    Added Laptop Class Properties

--- ClassLibrary/clsLaptops.cs
+++ ClassLibrary/clsLaptops.cs
@@ -1,6 +1,15 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsLaptops
     {
+        public string Model { get; set; }
+        public string Manufacturer { get; set; }
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+        public bool Reorder { get; set; }
+        public DateTime ReorderDate { get; set; }
+        public int ID { get; set; }
     }
 }
