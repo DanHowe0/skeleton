3ef68c986a17656a5d8484d510a6c322e800c2a5
Divyansh Singh <P2727346@my365.dmu.ac.uk>
2024-05-10 09:47:22 +0100

    Added a p[resentation layer and linked it with the middle layer

--- AdminSystem/StaffViewer.aspx.cs
+++ AdminSystem/StaffViewer.aspx.cs
@@ -4,11 +4,17 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1Viewer : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
-    {
+    {//create a new instance of clsStaff
+        clsStaff AnStaff = new clsStaff();
+        //get the data from the session object 
+        AnStaff = (clsStaff)Session["AnStaff"];
+        //display the StaffName for this entry
+        Response.Write(AnStaff.StaffName);
 
     }
 }
