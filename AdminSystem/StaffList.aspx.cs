e396c43e00c5a039457bf7266e6f7ce24262e6a7
Divyansh Singh <P2727346@my365.dmu.ac.uk>
2024-05-17 22:31:51 +0100

    created collection class

--- AdminSystem/StaffList.aspx.cs
+++ AdminSystem/StaffList.aspx.cs
@@ -4,11 +4,27 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1_List : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        if (IsPostBack == false)
+        {
+            DisplayAddress();
+        }
+    }
+    void DisplayAddress()
+    {
+        clsStaffCollection Staffs = new clsStaffCollection();
+        //set the data source to list of address in the collection 
+        lstStaffList.DataSource = Staffs.StaffList;
+        //set the primary key
+        lstStaffList.DataTextField = "StaffId";
+        //data field to display
+        lstStaffList.DataTextField = "StaffName";
+        //bind the data to list
+        lstStaffList.DataBind();
     }
 }
---
+++ ClassLibrary/clsStaffCollection.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassLibrary
+
+{
+    public class clsStaffCollection
+    {  public clsStaffCollection()
+        {
+            Int32 Index = 0;
+            Int32 RecordCount = 0;
+            clsDataConnection DB = new clsDataConnection();
+            DB.Execute("sproc_tblStaff_SelectAll");
+            RecordCount = DB.Count;
+            while(Index < RecordCount)
+            {
+                clsStaff AnStaff = new clsStaff();
+                AnStaff.StaffId = Convert.ToInt32(DB.DataTable.Rows[Index]["StaffId"]);
+                AnStaff.StaffAddress = Convert.ToString(DB.DataTable.Rows[Index]["StaffAddress"]);
+               AnStaff.StaffName = Convert.ToString(DB.DataTable.Rows[Index]["StaffName"]);
+               AnStaff.StaffPhoneNumber = Convert.ToString(DB.DataTable.Rows[Index]["StaffPhoneNumber"]);
+               AnStaff.StaffEmail = Convert.ToString(DB.DataTable.Rows[Index]["StaffEmail"]);
+               AnStaff.DateAdded = Convert.ToDateTime(DB.DataTable.Rows[Index]["DateAdded"]);
+               AnStaff.Active = Convert.ToBoolean(DB.DataTable.Rows[Index]["Active"]);
+                mStaffList.Add(AnStaff);    
+                Index++;    
+            }
+        }
+        List<clsStaff> mStaffList = new List<clsStaff>();
+        public List<clsStaff> StaffList
+        {
+            get
+            {
+                return mStaffList;
+            }
+            set
+            {
+                mStaffList = value;
+            }
+        }
+        public int Count
+        {
+            get { return mStaffList.Count; }
+            set { }
+        }
+       
+       
+    }
+}
