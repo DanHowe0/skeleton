370e015664d151eac48730d0306db843e43c914e
DanHowe0 <39921326+DanHowe0@users.noreply.github.com>
2024-05-28 21:50:33 +0100

    added add and edit functions

--- AdminSystem/LaptopsList.aspx.cs
+++ AdminSystem/LaptopsList.aspx.cs
@@ -31,4 +31,31 @@ public partial class _1_List : System.Web.UI.Page
         //bind the data to the list
         lstLaptopsList.DataBind();
     }
+
+    protected void btnAdd_Click(object sender, EventArgs e)
+    {
+        //store -1 into the session object to indicate that this is a new record
+        Session["LaptopId"] = -1;
+        //redirect to the data entry page
+        Response.Redirect("LaptopsDataEntry.aspx");
+    }
+
+    protected void btnEdit_Click(object sender, EventArgs e)
+    {
+        //variable to store the primarry key balue of the selected record
+        Int32 LaptopId;
+        //if a record has been selected from the list
+        if (lstLaptopsList.SelectedIndex != -1)
+        {
+            //get the primary key value of the record to edit
+            LaptopId = Convert.ToInt32(lstLaptopsList.SelectedValue);
+            //store the data in the session object
+            Session["LaptopId"] = LaptopId;
+            //redirect to the edit page
+            Response.Redirect("LaptopsDataEntry.aspx");
+        } else
+        {
+            lblError.Text = "Please select a record from the list to edit";
+        }
+    }
 }
--- Testing1/tstLaptopCollection.cs
+++ Testing1/tstLaptopCollection.cs
@@ -84,5 +84,73 @@ namespace Testing1
             //test to see that the two values are the same
             Assert.AreEqual(AllLaptops.Count, TestList.Count);
         }
+        [TestMethod]
+        public void AddMethodOK()
+        {
+            //create an instance of the class we want to create
+            clsLaptopsCollection AllLaptops = new clsLaptopsCollection();
+            //create the item of test data
+            clsLaptops TestItem = new clsLaptops();
+            //variable to store the primary key
+            Int32 PrimaryKey = 0;
+            //set the properties
+            TestItem.LaptopID = 1;
+            TestItem.LaptopModel = "Test";
+            TestItem.LaptopManufacturer = "Test";
+            TestItem.LaptopQuantity = 7357;
+            TestItem.LaptopPrice = 73.57;
+            TestItem.LaptopReorder = true;
+            TestItem.LaptopReorderDate = DateTime.Now.AddMonths(5);
+            //add the item to the test list
+            AllLaptops.ThisLaptop = TestItem;
+            //add the record
+            PrimaryKey = AllLaptops.Add();
+            //set the PrimaryKey of the test data
+            TestItem.LaptopID = PrimaryKey;
+            //find the record
+            AllLaptops.ThisLaptop.Find(PrimaryKey);
+            //test to see that the two values are the same
+            Assert.AreEqual(AllLaptops.ThisLaptop, TestItem);
+        }
+        [TestMethod]
+        public void UpdateMethodOK()
+        {
+            //create an instance of the class we want to create
+            clsLaptopsCollection AllLaptops = new clsLaptopsCollection();
+            //create the item of the test data
+            clsLaptops TestItem = new clsLaptops();
+            //variable to store the primary key
+            Int32 PrimaryKey = 0;
+            //set its proprties
+            TestItem.LaptopID = 1;
+            TestItem.LaptopModel = "Test";
+            TestItem.LaptopManufacturer = "Test";
+            TestItem.LaptopQuantity = 7357;
+            TestItem.LaptopPrice = 73.57;
+            TestItem.LaptopReorder = true;
+            TestItem.LaptopReorderDate = DateTime.Now.AddMonths(5);
+            //set ThisLaptops to the test data
+            AllLaptops.ThisLaptop = TestItem;
+            //add the record
+            PrimaryKey = AllLaptops.Add();
+            //set the primary key of the test data
+            TestItem.LaptopID = PrimaryKey;
+            //modify the test record
+            TestItem.LaptopID = PrimaryKey;
+            TestItem.LaptopModel = "Test 2";
+            TestItem.LaptopManufacturer = "Test 2";
+            TestItem.LaptopQuantity = 111;
+            TestItem.LaptopPrice = 12.31;
+            TestItem.LaptopReorder = true;
+            TestItem.LaptopReorderDate = DateTime.Now.AddMonths(5);
+            //set the record based on the new data
+            AllLaptops.ThisLaptop = TestItem;
+            //update the record
+            AllLaptops.Update();
+            //find the record
+            AllLaptops.ThisLaptop.Find(PrimaryKey);
+            //test to see if ThisAddress matches the rest data
+            Assert.AreEqual(AllLaptops.ThisLaptop, TestItem);
+        }
     }
 }
