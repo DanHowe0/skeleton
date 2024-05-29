28183ff174a40d5c7a4162475df3a6c414f88b94
Divyansh Singh <P2727346@my365.dmu.ac.uk>
2024-05-29 16:54:00 +0100

    login page

---
+++ AdminSystem/StaffLogin.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="StaffLogin.aspx.cs" Inherits="StaffLogin" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <p>
+            &nbsp;</p>
+        <p>
+            &nbsp;</p>
+        <p>
+            <asp:Label ID="lblStaff" runat="server" style="z-index: 1; left: 366px; top: 172px; position: absolute" Text="Staff Login Page"></asp:Label>
+        </p>
+        <asp:Label ID="lblUserName" runat="server" style="z-index: 1; left: 295px; top: 218px; position: absolute" Text="UserName"></asp:Label>
+        <asp:Label ID="lblPassword" runat="server" style="z-index: 1; left: 294px; top: 259px; position: absolute" Text="Password"></asp:Label>
+        <asp:TextBox ID="txtUserName" runat="server" style="z-index: 1; left: 408px; top: 218px; position: absolute"></asp:TextBox>
+        <p>
+            <asp:TextBox ID="txtPassword" runat="server" style="z-index: 1; left: 409px; top: 259px; position: absolute" TextMode="Password"></asp:TextBox>
+        </p>
+        <p>
+            <asp:Button ID="buttonLogIn" runat="server" height="26px" OnClick="buttonLogIn_Click" style="z-index: 1; left: 332px; top: 318px; position: absolute" Text="LogIn" width="60px" />
+        </p>
+        <p>
+            <asp:Button ID="ButtonCancel" runat="server" style="left: 451px; top: 317px; position: absolute; bottom: 322px" Text="Cancel" />
+        </p>
+        <p>
+            <asp:Label ID="lblError" runat="server" ForeColor="#FF3300" style="z-index: 1; left: 385px; top: 387px; position: absolute"></asp:Label>
+        </p>
+    </form>
+</body>
+</html>
---
+++ ClassLibrary/clsStaffUser.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Diagnostics.Eventing.Reader;
+
+namespace ClassLibrary
+{
+    public class clsStaffUser
+    {
+        private Int32 mUserId;
+        private String mUserName;
+        private String mPassword;
+        private String mDepartment;
+
+
+
+
+        public int UserId
+        {
+            get
+            {
+                return mUserId;
+            }
+            set
+            { 
+                mUserId = value;
+
+            }
+        }
+        public string UserName
+        {
+            get
+            {
+                return mUserName;
+            }
+            set
+            {
+                mUserName = value;
+            }
+        }
+        public string Password
+        {
+            get
+            {
+                return mPassword;
+            }
+            set 
+            {
+                mPassword = value;
+            }
+        }
+        public string Department
+        {
+            get
+            {
+                return mDepartment;
+
+            }
+            set
+            {
+                mDepartment = value;
+            }
+        }
+
+        public bool FindUser(string UserName, string Password)
+        {clsDataConnection DB = new clsDataConnection();
+            DB.AddParameter("@UserName", UserName);
+            DB.AddParameter("@Password", Password);
+            DB.Execute("sproc_tblUsers_FindStaffUserNamePW");
+            if (DB.Count == 1)
+            {
+                mUserId = Convert.ToInt32(DB.DataTable.Rows[0]["UserID"]);
+                mUserName = Convert.ToString(DB.DataTable.Rows[0]["UserName"]);
+                mPassword = Convert.ToString(DB.DataTable.Rows[0]["Password"]);
+                mDepartment = Convert.ToString(DB.DataTable.Rows[0]["Department"]);
+                return  true;
+
+            }
+            else
+            {
+                return false;   
+            }
+            
+        }
+        
+    }
+}
